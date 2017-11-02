0b1df492358ed3d43668f90678ebcdc6c07d7045
ding199309 <ding199309@163.com>
2017-11-02 15:04:13 +0800

    提交lambda

---
+++ src/com/demo/Major.java
@@ -0,0 +1,30 @@
+package com.demo;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author dfz
+ * E-mail:  dfz@jkinvest.cn
+ * @version 1.0
+ * @date 创建时间：2017/11/1 9:52
+ * @parameter
+ * @return
+ */
+public class Major {
+
+    private static AtomicInteger atomicInteger=new AtomicInteger(0);
+
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    atomicInteger.addAndGet(1);
+                    System.out.println(atomicInteger.get());
+                }
+            }).start();
+        }
+    }
+}
--- src/com/demo/java8/LambdaTest.java
+++ src/com/demo/java8/LambdaTest.java
@@ -10,5 +10,23 @@ package com.demo.java8;
 public class LambdaTest {
 
     public static void main(String[] args) {
+
+        Runnable r1=() -> System.out.println("hello world 1");
+
+        Runnable r2=new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("hello world 2");
+            }
+        };
+
+        process(r1);
+        process(r2);
+        process(() -> System.out.println("hello world 3"));
+
     }
+    public  static  void process(Runnable r){
+        r.run();
+    }
+
 }
