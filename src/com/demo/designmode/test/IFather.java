68a506b30ef8bf54a450d032b24ff658dd73eed8
woting <ding199309@163.com>
2017-12-13 22:55:35 +0800

    抽象工厂模式

---
+++ src/com/demo/designmode/test/IFather.java
@@ -0,0 +1,13 @@
+package com.demo.designmode.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: woting
+ * Date: 2017/12/13
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public interface IFather {
+
+    void test();
+}
