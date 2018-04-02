9074cef3b933f616e0df42b3486445b0172764b8
ding199309 <ding199309@163.com>
2018-04-02 17:22:47 +0800

    抽象工厂

---
+++ src/com/demo/array/Quick.java
@@ -0,0 +1,52 @@
+package com.demo.array;
+
+import java.util.Arrays;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author dfz
+ * @email dfz@jkinvest.cn
+ * @create 2018-03-13 16:36
+ * @since 1.0
+ */
+public class Quick{
+    public void sort(int arr[],int low,int high){
+        int l=low;
+        int h=high;
+        int povit=arr[low];
+        while(l<h){
+            while(l<h&&arr[h]>=povit) {
+                h--;
+            }
+            if(l<h){
+                int temp=arr[h];
+                arr[h]=arr[l];
+                arr[l]=temp;
+                l++;
+            }
+            while(l<h&&arr[l]<=povit) {
+                l++;
+            }
+            if(l<h){
+                int temp=arr[h];
+                arr[h]=arr[l];
+                arr[l]=temp;
+                h--;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+//        System.out.print("l="+(l+1)+"h="+(h+1)+"povit="+povit+"\n");
+        if(l>low) {
+            sort(arr, low, l - 1);
+        }
+        if(h<high) {
+            sort(arr, l + 1, high);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={3,1,6,2,7,9,5,0,8,4};
+        new Quick().sort(arr,0,9);
+    }
+}
